274a49b5079e0432edba8764e08ad574184be2c8
tienle270102 <letien27012002@gmail.com>
2022-06-27 08:46:16 +0700

    add file

---
+++ database/migrations/2022_06_24_014430_create_cars_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('model');
+            $table->string('description');
+            $table->date('produced_on');
+            $table->string('image');
+            $table->unsignedInteger('manufacturer_id');
+            $table->foreign('manufacturer_id')->references('id')->on('manufacturers')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+            $table->timestamps();
+        });
+        // Schema::table('cars', function (Blueprint $table) {
+            
+        //     $table->string('image');
+            
+        // });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+};
